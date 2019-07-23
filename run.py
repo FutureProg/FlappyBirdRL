060fadcafe14e14369c41e5397d387223b94985c
Nickolas A. Morrison <nickmorrison09@gmail.com>
2019-07-22 22:32:33 -0400

    Update run.py and add recording

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import gym
 import flappy_bird
 
 from stable_baselines import PPO2
-from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.common.vec_env import SubprocVecEnv
 
-env = DummyVecEnv([lambda: gym.make('flappy_bird-v0')])
+env = SubprocVecEnv([lambda: gym.make('flappy_bird-v0')])
 model = PPO2.load('ppo2_flappybird')
 obs = env.reset()
 while True:
